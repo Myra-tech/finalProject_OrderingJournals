1a023dbf912de26a53b42b5e8daf11184e578493
Myra-tech <ance.l.tetere@gmail.com>
2022-03-08 14:30:23 +0200

    Now the book entries also create an object, parameters of which are renounced in the book page. The code holds together.

--- src/main/java/com/java_24_25/finalProject/controllers/BookController.java
+++ src/main/java/com/java_24_25/finalProject/controllers/BookController.java
@@ -1,17 +1,30 @@
 package com.java_24_25.finalProject.controllers;
 
+import com.java_24_25.finalProject.models.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BookController {
     @GetMapping("/create")
     public String showCreateTheBookPage(Model model) {
-//        Šim būtu jādarbojas /the_book lapā
-        model.addAttribute("coverType", "hardcover");
-        model.addAttribute("leatherType", "vegetable tanned");
-        model.addAttribute("colourOfLeather", "black");  // colour hex black #000
+        System.out.println("The user is creating a book.");
+
+
         return "createTheBook";
     }
 }
+
+//@Controller
+//public class BookController {
+//    @GetMapping("/create")
+//    public String showCreateTheBookPage(Model model) {
+////        Šim būtu jādarbojas /the_book lapā
+//        model.addAttribute("coverType", "hardcover");
+//        model.addAttribute("leatherType", "vegetable tanned");
+//        model.addAttribute("colourOfLeather", "black");  // colour hex black #000
+//        return "createTheBook";
+//    }
+
--- src/main/java/com/java_24_25/finalProject/controllers/CustomerDetailController.java
+++ src/main/java/com/java_24_25/finalProject/controllers/CustomerDetailController.java
@@ -1,20 +1,35 @@
 package com.java_24_25.finalProject.controllers;
 
 
+import com.java_24_25.finalProject.models.Book;
 import com.java_24_25.finalProject.services.PageDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerDetailController {
 
     @Autowired
     PageDataService pageDataService;
+    BookController bookController;
 
     @GetMapping("/the_book")
-    public String showTheBookPage(Model model) {
+    public String showTheBookPage(Model model,
+                                  @RequestParam(name = "coverType", required = true) String coverType,
+                                  @RequestParam(name = "leatherType") String leatherType,
+                                  @RequestParam(name = "colourOfLeather") String colourOfLeather,
+                                  @RequestParam(name = "size") String size,
+                                  @RequestParam(name = "numberOfPages") int numberOfPages,
+                                  @RequestParam(name = "paperType") String paperType,
+                                  @RequestParam(name = "paperBaseColour") String paperBaseColour,
+                                  @RequestParam(name = "vintageLook", required = false) String vintageLook,
+                                  @RequestParam(name = "coverPrints", required = false) String coverPrints,
+                                  @RequestParam(name = "additionalStyleElements", required = false) String additionalStyleElements,
+                                  @RequestParam(name = "customerWishes", required = false) String customerWishes
+    ) {
 
         System.out.println(pageDataService.getProjectTitle()); // shows in the console that it works
 
@@ -23,6 +38,30 @@ public class CustomerDetailController {
         model.addAttribute("pageInfo", pageDataService.getPage("yourBook"));
         model.addAttribute("availablePages", pageDataService.getPages());
         // then html can use this to display a page "yourBook"
+
+        System.out.println("Book's input parameters are being extracted." + '\''
+                + "Cover type: " + coverType + '\''
+                + "Leather type: " + leatherType + '\''
+                + "Colour of the leather: " + colourOfLeather + '\''
+                + "Size of the book: " + size + '\''
+                + "Number of pages: " + numberOfPages + '\''
+                + "Paper type: " + paperType + '\''
+                + "Paper base colour: " + paperBaseColour + '\''
+                + "Vintage look: " + vintageLook + '\''
+                + "Cover prints: " + coverPrints + '\''
+                + "Additional style elements: " + additionalStyleElements + '\''
+                + "Customer wishes: " + customerWishes);
+
+        Book book = new Book(coverType, leatherType, colourOfLeather, size, numberOfPages, paperType, paperBaseColour, coverPrints, additionalStyleElements);
+        model.addAttribute("coverType", coverType);
+        model.addAttribute("leatherType", leatherType);
+        model.addAttribute("colourOfLeather", colourOfLeather);
+        model.addAttribute("size", size);
+        model.addAttribute("numberOfPages", numberOfPages);
+        model.addAttribute("paperType", paperType);
+        model.addAttribute("paperBaseColour", paperBaseColour);
+        model.addAttribute("coverPrints", coverPrints);
+        model.addAttribute("additionalStyleElements", additionalStyleElements);
 
         return "yourBook";
     }
